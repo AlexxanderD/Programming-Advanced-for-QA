1d0c96750861a924e941b480d767c702fc2258b8
Aleksandar <astanchev95@gmail.com>
2024-02-17 12:14:05 +0200

    update

---
+++ Unit Testing Exercise Dictionaries, Lambda and LINQ/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/OrdersTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class OrdersTests
+{
+    [Test]
+    public void Test_Order_WithEmptyInput_ShouldReturnEmptyString()
+    {
+        // Arrange
+        string[] input = new string[0];
+        // Act
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_Order_WithMultipleOrders_ShouldReturnTotalPrice()
+    {
+        // Arrange
+        string[] input = new string[]
+        {
+            "apple 1.99 1" ,
+            "banana 0.75 5",
+            "orange 0.99 2",
+            "apple 1.99 2"
+        };
+        // Act
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"apple -> 5.97{Environment.NewLine}banana -> 3.75{Environment.NewLine}orange -> 1.98"));
+    }
+
+    [Test]
+    public void Test_Order_WithRoundedPrices_ShouldReturnTotalPrice()
+    {
+        // Arrange
+        string[] input = new string[]
+        {
+            "apple 1.990000002 1" ,
+            "banana 0.7500003 5",
+            "orange 0.990006 2",
+            "apple 1.990004 2"
+        };
+        // Act
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"apple -> 5.97{Environment.NewLine}banana -> 3.75{Environment.NewLine}orange -> 1.98"));
+    }
+
+    [Test]
+    public void Test_Order_WithDecimalQuantities_ShouldReturnTotalPrice()
+    {
+        // Arrange
+        string[] input = new string[]
+        {
+            "apple 2.00 4.5" ,
+            "banana 3.00 2.5",
+            "orange 1.00 3.5",
+          
+        };
+        // Act
+        string result = Orders.Order(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo($"apple -> 9.00{Environment.NewLine}banana -> 7.50{Environment.NewLine}orange -> 3.50"));
+    }
+}
---
+++ Unit Testing Exercise Dictionaries, Lambda and LINQ/06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/PlantsTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.Tests;
+
+public class PlantsTests
+{
+    [Test]
+    public void Test_GetFastestGrowing_WithEmptyArray_ShouldReturnEmptyString()
+    {
+        // Arrange
+        string[] plants = new string[0];
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+    [Test]
+    public void Test_GetFastestGrowing_WithSinglePlant_ShouldReturnPlant()
+    {
+        // Arrang
+        string[] plants = new string[]
+        {
+            "@--\\-"
+        };
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+        // Assert
+        Assert.That(result, Is.EqualTo("Plants with 5 letters:\r\n@--\\-"));
+    }
+
+    [Test]
+    public void Test_GetFastestGrowing_WithMultiplePlants_ShouldReturnGroupedPlants()
+    {
+        // Arrang
+        string[] plants = new string[]
+        {
+            "@--\\-",
+            "*---",
+            "____",
+            "*-\\-\\"
+        };
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+        // Assert
+        Assert.That(result, Is.EqualTo("Plants with 4 letters:\r\n*---\r\n____\r\n" +
+            "Plants with 5 letters:\r\n@--\\-\r\n*-\\-\\"));
+    }
+
+    [Test]
+    public void Test_GetFastestGrowing_WithMixedCasePlants_ShouldBeCaseInsensitive()
+    {
+        // Arrang
+        string[] plants = new string[]
+        {
+            "rOse",
+            "liLy",
+            "AbcdE",
+            "Fdgre"
+        };
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+        // Assert
+        Assert.That(result, Is.EqualTo("Plants with 4 letters:\r\nrOse\r\nliLy\r\n" +
+            "Plants with 5 letters:\r\nAbcdE\r\nFdgre"));
+    }
+}
